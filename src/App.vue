96a216873ac31647ef544bf3a6208ac45bdd929a
Laetitia Bousch <laetitia.bousch@epitech.eu>
2021-07-11 20:21:36 +0200

    [FIX] win

--- src/App.vue
+++ src/App.vue
@@ -229,7 +229,7 @@ export default {
         console.log("win");
         this.win = true;
       }
-      if (this.life_bubble <= 0.5) {
+      if (this.life_bubble <= 0) {
         console.log("dead");
         this.lose = true;
       }
